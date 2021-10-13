e9a37f127b2c889df161418c878162c6dbf0bc96
AcydAce <bayuhartho@gmail.com>
2021-10-13 21:33:12 +0700

    Update Algo n Prog Forum Assignment 1.py

--- Algo n Prog/Algo n Prog Forum Assignment 1.py
+++ Algo n Prog/Algo n Prog Forum Assignment 1.py
@@ -9,7 +9,34 @@ denominator = int(input("insert denominator : "))
 while denominator < 0:
     denominator = int(input("denominator cannot be <= 0,insert numerator [>0] : "))
 
+fraction = (f"{numerator} / {denominator}")
 
+if numerator < denominator:
 
+    fraction_type = "proper fraction"
+    print(f"[{fraction}] is a proper fraction.")
+else:
 
+    fraction_type = "improper fraction"
+    print(f"[{fraction}] is an improper fraction.")
 
+gcd      = math.gcd(numerator, denominator)
+
+if gcd == 1:
+
+    print(f"This {fraction_type} cannot be simplified.")
+
+else:
+
+    numerator = numerator // gcd
+    denominator = denominator // gcd
+
+    print(f"This {fraction_type} can be simplified into: {numerator} / {denominator}")
+
+WholeNum = numerator // denominator
+Remainder = numerator % denominator
+
+if Remainder == 0:
+    print(f"{fraction} can be written as the whole number {WholeNum}.")
+else:
+    print(f"{fraction} can be written as the mixed number {WholeNum} {Remainder} / {Denom}.")
