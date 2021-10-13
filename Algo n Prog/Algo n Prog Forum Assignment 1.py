eb7d6f419e01d71202de084e123070f278e83b04
AcydAce <bayuhartho@gmail.com>
2021-10-13 11:46:49 +0700

    Create Algo n Prog Forum Assignment 1.py

---
+++ Algo n Prog/Algo n Prog Forum Assignment 1.py
@@ -0,0 +1,14 @@
+
+
+numerator = 0
+while numerator < 0:
+    numerator = eval(input("insert numerator [>0] : "))
+
+denominator = 0
+while denominator < 0:
+    denominator = int(input("insert numerator [>0] : "))
+
+print("sugoi")
+
+
+
