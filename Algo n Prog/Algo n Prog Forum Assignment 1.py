94edf40ec4fb0e3802de9805bbebbeb9ba206670
AcydAce <bayuhartho@gmail.com>
2021-10-13 20:22:30 +0700

    Update Algo n Prog Forum Assignment 1.py

--- Algo n Prog/Algo n Prog Forum Assignment 1.py
+++ Algo n Prog/Algo n Prog Forum Assignment 1.py
@@ -1,14 +1,15 @@
 
+import math
 
-numerator = 0
-while numerator < 0:
-    numerator = eval(input("insert numerator [>0] : "))
+numerator = int(input("insert numerator : "))
+while numerator <= 0:
+    numerator = int(input("numerator cannot be <= 0, insert numerator [>0] : "))
 
-denominator = 0
+denominator = int(input("insert denominator : "))
 while denominator < 0:
-    denominator = int(input("insert numerator [>0] : "))
+    denominator = int(input("denominator cannot be <= 0,insert numerator [>0] : "))
 
-print("sugoi")
+
 
 
 
